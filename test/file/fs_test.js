23a33ea012d65c42b16096a46865866886474b65
Takumi Endo <tq-ed@v7.com>
2014-06-07 04:49:02 +0900

    fs.readFile, fs.readFileSyncの確認

--- test/file/fs_test.js
+++ test/file/fs_test.js
@@ -1,10 +1,52 @@
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
 
 var fixture_path = "./test/fixtures/fs_test";
 
 describe('fs', function() {
+  describe('readFile', function() {
+    context('encodintオプションを渡した場合、', function() {
+      it('ファイルの中身をStringで返す', function(done) {
+        fs.readFile(path.join(fixture_path, 'foo.txt'), { encoding: 'utf8' }, function(err, data) {
+          assert(err === null);
+          assert(data === "foo\n");
+          done();
+        });
+      });
+    });
+
+    context("存在しないファイルパスを渡した場合、", function() {
+      it('Error::ENOENTエラーを返す', function(done) {
+        fs.readFile(path.join(fixture_path, 'not_found'), function(err, data) {
+          assert(err.code === "ENOENT");
+          done();
+        });
+      });
+    });
+  });
+
+  describe('readFileSync', function() {
+    context("encodingオプションを渡した場合、", function() {
+      it('ファイルの中身をStringで返す', function() {
+        var data = fs.readFileSync(path.join(fixture_path, 'foo.txt'), { encoding: 'utf8' });
+        assert(data === "foo\n");
+      });
+    });
+
+    context("存在しないファイルパスを渡した場合、", function() {
+      it('Error.ENOENTがスローされる', function() {
+        try {
+          fs.readFileSync(path.join(fixture_path, 'not_found'));
+          assert(1 === 0);
+        } catch(e) {
+          assert(e.code === "ENOENT");
+        }
+      });
+    });
+  });
+
   describe('readdir', function() {
-    it('should return an array of paths in the directory asynchronously', function(done) {
+    it('ディレクトリにあるファイルパスを配列で返す', function(done) {
       fs.readdir(fixture_path, function(err, files) {
         assert(files.indexOf('foo.txt') === 0);
         assert(files.indexOf('xxx.txt') < 0);
@@ -14,7 +56,7 @@ describe('fs', function() {
   });
 
   describe('readdirSync', function() {
-    it('should return an array of paths in the directory', function() {
+    it('ディレクトリにあるファイルパスを配列で返す', function() {
       var files = fs.readdirSync(fixture_path);
 
       assert(files.indexOf('foo.txt') === 0);
