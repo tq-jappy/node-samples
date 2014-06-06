955cbdad53180bd4410289f91c9d5017f6540767
Takumi Endo <tq-ed@v7.com>
2014-06-07 07:20:55 +0900

    update

--- test/enable-power-assert.js
+++ test/enable-power-assert.js
@@ -3,21 +3,21 @@ require('espower-loader')({
   cwd: process.cwd(),
   pattern: 'test/**/*.js',
 
-  // esPowerOptions: {
-  //   powerAssertVariableName: 'assert',
-  //   targetMethods: {
-  //     oneArg: [
-  //       'ok'
-  //     ],
-  //     twoArgs: [
-  //       'equal',
-  //       'notEqual',
-  //       'strictEqual',
-  //       'notStrictEqual',
-  //       'deepEqual',
-  //       'notDeepEqual'
-  //     ]
-  //   }
-  // }
+  esPowerOpetions: {
+    powerAssertVariableName: 'assert',
+    targetMethods: {
+      oneArg: [
+        'ok'
+      ],
+      twoArgs: [
+        'equal',
+        'notEqual',
+        'strictEqual',
+        'notStrictEqual',
+        'deepEqual',
+        'notDeepEqual'
+      ]
+    }
+  }
 });
 
